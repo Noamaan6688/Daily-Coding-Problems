63d85a99812ada7f1cb2d78812fbdd7935e8848f
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:51:05 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1553A | Digits Sum/164058001.cpp
@@ -0,0 +1,31 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long 
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ flash
+ int r;cin>>r;while (r--)
+ {
+  ll k;cin>>k;
+  cout<<(k+1)/10<<"\n";
+ }
+ 
+ return 0;
+}
+/*
+5
+1
+9
+10
+34
+880055535
+ 
+*/
