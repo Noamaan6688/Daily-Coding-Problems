babb084a5f711d8c5a1cff3b0fc5a515b3f71fe6
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:55:22 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1594A | Consecutive Sum Riddle/162473122.cpp
@@ -0,0 +1,33 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ flash
+ int r;cin>>r;while (r--)
+ {
+  ll k;cin>>k;
+  cout<<-k+1<<" "<<k<<"\n";
+ }
+ 
+ return 0;
+}
+/*
+7
+1
+2
+3
+6
+100
+25
+3000000000000
+ 
+*/
