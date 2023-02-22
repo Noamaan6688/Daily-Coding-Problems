a75636f6a733d551c9b5919b68bbc62bf614a374
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:46:23 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1516A | Tit for Tat/166019378.cpp
@@ -0,0 +1,39 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i=0;i<n;i++)
+#define f1(i, n) for (int i=1;i<=n;i++)
+#define fn(i, j, k) for (int i=j;i<=k;i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ flash
+ int r;cin>>r;while (r--)
+ {
+    int p,q;
+  cin>>p>>q;
+  int b[p];
+  f1(i,p) cin>>b[i];
+  int i=1; 
+  while (q--) 
+    {
+   while (b[i]==0 && i!=p) i+=1;
+   b[i]-=1;b[p]+=1;
+  }
+  f1(i,p) cout<<b[i]<<" ";cout<<"\n";
+  }
+ 
+  return 0;
+}
+/*
+2
+3 1
+3 1 4
+2 10
+1 0
+ 
+*/
