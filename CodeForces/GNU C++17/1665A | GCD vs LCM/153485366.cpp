d2c097d7a047d51cf2897e23656c7d1a524c726d
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 20:12:19 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1665A | GCD vs LCM/153485366.cpp
@@ -0,0 +1,31 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+  flash
+  int r;cin>>r;while (r--)
+  {
+    ll k;cin>>k;
+    cout<<k-3<<" "<<1<<" "<<1<<" "<<1<<"\n";
+  }
+ 
+  return 0;
+}
+/*
+5
+4
+7
+8
+9
+10
+ 
+*/
