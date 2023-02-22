cbce4bf9a913c8224159ac6a26a4520cf89d59e6
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 20:15:23 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1646B | Quality vs Quantity/148443648.cpp
@@ -0,0 +1,44 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+  flash
+  ll r;cin>>r;while (r--)
+  {
+    ll k;cin>>k;
+    ll b[k];
+    f(i,k)cin>>b[i];
+    sort(b,b+k);
+    ll s1=b[0],s2=0,i=1,j=k-1;
+    while (i<j)
+    {
+      s1+=b[i];s2+=b[j];
+      i+=1;j-=1;
+    }
+    if (s2>s1) cout<<"YES\n";
+    else cout<<"NO\n";
+  }
+ 
+  return 0;
+}
+/*
+4
+3
+1 2 3
+5
+2 8 6 3 1
+4
+3 5 4 2
+5
+1000000000 1000000000 1000000000 1000000000 1000000000
+ 
+*/
