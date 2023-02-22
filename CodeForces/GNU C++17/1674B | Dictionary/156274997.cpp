7d6d02974cb6cc94540a0f408588a3e8f1bc3744
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 20:11:37 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1674B | Dictionary/156274997.cpp
@@ -0,0 +1,37 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fn(i, j, k) for (int i = j; i <= k; i++)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+  flash
+  int r;cin>>r;while (r--)
+  {
+    string s;cin>>s;
+    int c=0;
+    if ((s[0]-'a')>0) c+=((s[0]-'a'))*25;
+    if (s[0]>s[1]) c+=(s[1]-'a')+1;
+    else c+=(s[1]-'a');
+    cout<<c<<"\n";
+  }
+ 
+  return 0;
+}
+/*
+7
+ab
+ac
+az
+ba
+bc
+zx
+zy
+ 
+*/
