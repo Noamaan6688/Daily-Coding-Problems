07cfa6ce690a8f6655020cab8f97f9b6c5e6e5cc
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:34:43 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1743B | Permutation Value/176721128.cpp
@@ -0,0 +1,41 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fn(i, j, k) for (int i = j; i <= k; i++)
+#define maan  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+void solve()
+{
+  int k;cin>>k;
+  cout<<"1 "<<k<<" ";
+  for (int i=2;i<k;i+=1)
+  {
+    cout<<i<<" ";
+  }
+  cout<<"\n";
+ 
+  return;
+}
+ 
+ 
+int main()
+{
+  maan
+  int r;cin>>r;while (r--)
+  {
+    solve();
+  }
+ 
+  return 0;
+}
+/*
+2
+5
+6
+ 
+*/
