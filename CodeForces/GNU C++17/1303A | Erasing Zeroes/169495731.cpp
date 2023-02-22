1e9bb6bb518c455eefc80c4a6502e4c40350bcd3
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:41:42 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1303A | Erasing Zeroes/169495731.cpp
@@ -0,0 +1,45 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i=0;i<n;i++)
+#define f1(i, n) for (int i=1;i<=n;i++)
+#define fn(i, j, k) for (int i=j;i<=k;i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ flash
+ int r;cin>>r;while (r--)
+ {
+  string s;cin>>s;
+  int c=0,d=-1,e=-1;
+  for (int i=0;i<s.size();i+=1)
+  {
+   if (s[i]=='1')
+   {
+    if (d==-1) d=i;
+    else e=i;
+   } 
+  }
+  for (int i=d;i<e;i+=1)
+  {
+   if (s[i]=='0') c+=1;
+  }
+  cout<<c<<"\n";
+ }
+ 
+  return 0;
+}
+/*
+4
+010011
+0
+1111000
+100111000000
+ 
+*/
+ 
+    
