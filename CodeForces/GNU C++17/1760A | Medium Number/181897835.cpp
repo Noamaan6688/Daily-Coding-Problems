fe905428f4ba5bdd3c28b22b2a1f65bc6ff8bc35
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:32:25 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1760A | Medium Number/181897835.cpp
@@ -0,0 +1,43 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fn(i, j, k) for (int i = j; i <= k; i++)
+#define maan  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+void solve()
+{
+  int b[3];f(i,3) cin>>b[i];
+  sort(b,b+3);
+  cout<<b[1]<<"\n";
+ 
+  return;
+}
+ 
+int main()
+{
+  maan
+  int r;cin>>r;while (r--)
+  {
+    solve();
+  }
+ 
+  return 0;
+}
+/*
+9
+5 2 6
+14 3 4
+20 2 1
+1 2 3
+11 19 12
+10 8 20
+6 20 3
+4 1 3
+19 8 4
+ 
+*/
