36b6507e650f58d0b8b0593e7dc578879e93faa6
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 20:06:07 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1682A | Palindromic Indices/158395295.cpp
@@ -0,0 +1,39 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fn(i, j, k) for (int i = j; i <= k; i++)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+  flash
+  int r;cin>>r;while (r--)
+  {
+    int k,c=0;cin>>k;
+    string s;cin>>s;
+    fn(i,(k+1)/2,k-1)
+    {
+      if (s[i]!=s[k/2]) break;
+      c+=2;
+    }
+    c+=k%2;
+    cout<<c<<"\n";
+  }
+ 
+  return 0;
+}
+/*
+3
+3
+aba
+8
+acaaaaca
+2
+dd
+ 
+*/
