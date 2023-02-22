e37d3b31e20b97ea5463ddd66660dcd7d0a74b35
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:27:09 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1698A | XOR Mixup/193290466.cpp
@@ -0,0 +1,43 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fn(i, j, k) for (int i = j; i <= k; i++)
+#define maan  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+void solve()
+{
+  int k;cin>>k;
+  int b[k];
+  f(i,k) cin>>b[i];
+  cout<<b[0]<<"\n";
+ 
+  return;
+}
+ 
+int main()
+{
+  maan
+  int r;cin>>r;while (r--)
+  {
+    solve();
+  }
+ 
+  return 0;
+}
+/*
+4
+4
+4 3 2 5
+5
+6 1 10 7 10
+6
+6 6 6 6 6 6
+3
+100 100 0
+ 
+*/
