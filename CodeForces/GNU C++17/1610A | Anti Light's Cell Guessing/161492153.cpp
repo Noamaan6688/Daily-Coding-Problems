469e1cd2bfcf739b5aedcf2e3fb0b017576d4901
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:57:51 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1610A | Anti Light's Cell Guessing/161492153.cpp
@@ -0,0 +1,30 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ flash
+ int r;cin>>r;while (r--)
+ {
+  ll p,q;cin>>p>>q;
+  if (p==1 && q==1) cout<<"0\n";
+  else if (p==1 || q==1) cout<<"1\n";
+  else cout<<"2\n";
+ }
+ 
+ return 0;
+}
+/*
+2
+2 3
+3 1
+ 
+*/
