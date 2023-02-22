2696d4e57cb967f1bd7a875b7b6912b310f3e5f2
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:48:35 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1537B | Bad Boy/165036165.cpp
@@ -0,0 +1,32 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long 
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ int r;cin>>r;while (r--)
+ {
+  ll a,b,c,d;cin>>a>>b>>c>>d;
+  cout<<1<<" "<<1<<" "<<a<<" "<<b<<"\n";
+ }
+ 
+  return 0;
+}
+/*
+7
+2 3 1 1
+4 4 1 2
+3 5 2 2
+5 1 2 1
+3 1 3 1
+1 1 1 1
+1000000000 1000000000 1000000000 50
+ 
+*/
