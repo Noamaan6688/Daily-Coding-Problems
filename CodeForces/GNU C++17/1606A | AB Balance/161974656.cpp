154c28e1c6592010f2af0f397b317ffedd23efe1
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:56:15 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1606A | AB Balance/161974656.cpp
@@ -0,0 +1,31 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fn(i, j, k) for (int i = j; i <= k; i++)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+  flash
+  int r;cin>>r;while (r--)
+  {
+    string s;cin>>s;
+    s[0]=s[s.size()-1];
+    cout<<s<<"\n";
+  }
+ 
+  return 0;
+}
+/*
+4
+b
+aabbbabaa
+abbb
+abbaab
+ 
+*/
