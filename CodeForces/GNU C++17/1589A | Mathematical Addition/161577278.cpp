054dbf513ba42267f2744e40bb2eedea6e9a15bc
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:57:41 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1589A | Mathematical Addition/161577278.cpp
@@ -0,0 +1,30 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ flash
+ int r;cin>>r;while (r--)
+ {
+  ll p,q;cin>>p>>q;
+  cout<<-p*p<<" "<<q*q<<"\n";
+ }
+ 
+ return 0;
+}
+/*
+4
+1 1
+2 3
+3 5
+6 9
+ 
+*/
