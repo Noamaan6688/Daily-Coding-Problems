43ef83daa9e08ae68cbf4051248e9c090dff5c04
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:40:28 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1430C | Numbers on Whiteboard/169794108.cpp
@@ -0,0 +1,43 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fn(i, j, k) for (int i = j; i <= k; i++)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+void solve()
+{
+  int k,c=0;cin>>k;
+  c=k;
+  cout<<"2\n";
+  cout<<k<<" "<<k-1<<"\n";
+  f(i,k-2)
+  {
+    cout<<c<<" "<<c-2<<"\n";
+    c-=1;
+  }
+ 
+  return;
+}
+ 
+ 
+int main()
+{
+  flash
+  int r;cin>>r;while (r--)
+  {
+    solve();
+  }
+ 
+  return 0;
+}
+/*
+2
+8
+7
+ 
+*/
