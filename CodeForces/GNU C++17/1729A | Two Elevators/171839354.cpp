d41f5f92d63b9ad7f92d7472b5706591c5ede9a0
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:38:46 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1729A | Two Elevators/171839354.cpp
@@ -0,0 +1,39 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define fn(i, j, k) for (int i = j; i <= k; i++)
+#define flash ios_base::sync_with_stdio(0);cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+void solve()
+{
+  int a,b,c;cin>>a>>b>>c;
+  if ((a-1)==(abs(b-c)+(c-1))) cout<<"3\n";
+  else if ((a-1)<(abs(b-c)+(c-1))) cout<<"1\n";
+  else cout<<"2\n";
+ 
+  return;
+}
+ 
+ 
+int main()
+{
+  flash
+  int r;cin>>r;while (r--)
+  {
+    solve();
+  }
+ 
+  return 0;
+}
+/*
+3
+1 2 3
+3 1 2
+3 2 1
+ 
+*/
