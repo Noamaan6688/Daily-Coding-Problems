62251760a624473140cee51767aa0e7a6bbb45a6
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:51:28 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1555A | PizzaForces/163403956.cpp
@@ -0,0 +1,32 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long 
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ flash
+ int r;cin>>r;while (r--)
+ {
+  ll k,c=6;cin>>k;
+        cout<<max(c,k+1)/2*5<<"\n";
+ }
+ 
+ return 0;
+}
+/*
+6
+12
+15
+300
+1
+9999999999999999
+3
+ 
+*/
