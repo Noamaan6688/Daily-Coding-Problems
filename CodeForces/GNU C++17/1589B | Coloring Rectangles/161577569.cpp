682eae3fd00c7018fa82f4a76440919ff3fef771
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-22 19:57:36 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1589B | Coloring Rectangles/161577569.cpp
@@ -0,0 +1,30 @@
+// Hi
+#include <bits/stdc++.h>
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define f1(i, n) for (ll i = 1; i <= n; i++)
+#define fn(i, j, k) for (ll i = j; i <= k; i++)
+#define flash  ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long int
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main()
+{
+ flash
+ int r;cin>>r;while (r--)
+ {
+  int p,q;cin>>p>>q;
+  cout<<(p*q+2)/3<<"\n";
+ }
+ 
+ return 0;
+}
+/*
+4
+1 3
+2 2
+2 5
+3 5
+ 
+*/
