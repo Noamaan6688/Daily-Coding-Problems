b4051a15f9c20cd294d4ee1edfd48ed5258c5d28
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-26 10:56:25 +0530

    Time: 12 ms (71.88%), Space: 6.4 MB (93.87%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int minDistance(string w1, string w2) {
         int n1=w1.size(),n2=w2.size();
-        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
-        for (int i=0;i<=n1;i+=1) dp[i][0]=i;
-        for (int  j=0;j<=n2;j+=1) dp[0][j]=j;
+        vector<int>prev(n2+1,0),curr(n2+1,0);
+        for (int i=0;i<=n2;i+=1) prev[i]=i;
         for (int i=1;i<=n1;i+=1)
         {
+            curr[0]=i;
             for (int j=1;j<=n2;j+=1)
             {
-                if (w1[i-1]!=w2[j-1]) dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
-                else dp[i][j]=dp[i-1][j-1];
+                if (w1[i-1]!=w2[j-1]) curr[j]=min({prev[j],curr[j-1],prev[j-1]})+1;
+                else curr[j]=prev[j-1];
             }
+            prev=curr;
         }
-        return dp[n1][n2];
+        return prev[n2];
     }
 };
