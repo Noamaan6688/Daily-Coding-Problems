e6b5abc26f7fed2a7eb570f0dbdfedaa12b803b3
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-02-28 22:26:59 +0530

    Time: 3 ms, Memory: 17.3 MB - LeetHub

--- symmetric-tree/symmetric-tree.cpp
+++ symmetric-tree/symmetric-tree.cpp
@@ -10,21 +10,44 @@
  * };
  */
 class Solution {
-public:
-    bool df(TreeNode*l,TreeNode*r)
-    {
-        if (l==NULL && r==NULL) return 1;
-        if (l==NULL || r==NULL) return 0;
-        if (l->val!=r->val) return 0;
-        bool t1=df(l->left,r->right);
-        bool t2=df(l->right,r->left);
-        if (t1==0 || t2==0) return 0;
-        return 1;
-    }
-    
-    
+public:  
     bool isSymmetric(TreeNode* root) {
-        bool t=df(root->left,root->right);
-        return t;
+        vector<vector<int>>v;
+        queue<TreeNode*>q;
+        q.push(root);
+        while (!q.empty())
+        {
+            vector<int>v1;
+            int n=q.size();
+            while (n--)
+            {
+                TreeNode* node=q.front();
+                if (node->left)
+                {
+                    q.push(node->left);
+                    v1.push_back(node->left->val);
+                }
+                else v1.push_back(-101);
+                if (node->right)
+                {
+                    q.push(node->right);
+                    v1.push_back(node->right->val);
+                }
+                else v1.push_back(-101);
+                q.pop();
+            }
+            v.push_back(v1);        
+        }
+        for (int i=0;i<v.size();i+=1)
+        {
+            int n=v[i].size();
+            int n1=n/2-1,n2=n/2;
+            while (n1>=0 && n2<n)
+            {
+                if (v[i][n1]!=v[i][n2]) return 0;
+                n1-=1;n2+=1;
+            }
+        }
+        return 1;
     }
 };
