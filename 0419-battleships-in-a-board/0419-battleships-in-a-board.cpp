5681d99266744f448a63af2850e74240ceb3a0f3
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-10 23:50:32 +0530

    Time: 4 ms (96.41%), Space: 8.2 MB (24.85%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void df(int i,int j,vector<vector<char>>&grid)
-    {
-        int n=grid.size(),m=grid[0].size();
+//     void df(int i,int j,vector<vector<char>>&grid)
+//     {
+//         int n=grid.size(),m=grid[0].size();
         
-        if (i<0 || j<0 || i>=n || j>=m || grid[i][j]!='X') return;
-        grid[i][j]='Y';
-        df(i+1,j,grid);
-        df(i,j+1,grid);
-    }
+//         if (i<0 || j<0 || i>=n || j>=m || grid[i][j]!='X') return;
+//         grid[i][j]='Y';
+//         df(i+1,j,grid);
+//         df(i,j+1,grid);
+//     }
     
     int countBattleships(vector<vector<char>>& grid) {
         int n=grid.size(),m=grid[0].size(),c=0;
-        for (int i=0;i<n;i+=1)
+        for (int i=n-1;i>=0;i-=1)
         {
-            for (int j=0;j<m;j+=1)
+            for (int j=m-1;j>=0;j-=1)
             {
-                if (grid[i][j]=='X')
-                {
-                    df(i,j,grid);
-                    c+=1;
-                }
+                if (grid[i][j]=='.') continue;
+                if (i<n-1 && grid[i+1][j]=='X') continue;
+                if (j<m-1 && grid[i][j+1]=='X') continue;
+                c+=1;
             }
         }
         return c;
