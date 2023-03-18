0fe4f0a49c635c176228ed19abb882ae94bc5361
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-18 23:46:57 +0530

    Time: 96 ms (45.94%), Space: 34.5 MB (28.95%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        vector<vector<int>>dp(mat.size(),vector<int>(mat[0].size(),1e9));
-        for (int i=0;i<mat.size();i+=1)
+        int n=mat.size(),m=mat[0].size();
+        int v1[]={-1,0,1,0},v2[]={0,1,0,-1};
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>visit(n,vector<int>(m,0));
+        vector<vector<int>>v(n,vector<int>(m,0));
+        for (int i=0;i<n;i+=1)
         {
-            for (int j=0;j<mat[0].size();j+=1)
+            for (int j=0;j<m;j+=1)
             {
-                if (mat[i][j]==0) dp[i][j]=0;
-                else
+                if (mat[i][j]==0) q.push({{i,j},0}),visit[i][j]=1;
+            }
+        }
+        while (q.size()!=0)
+        {
+            int i=q.front().first.first,j=q.front().first.second,c=q.front().second;
+            q.pop();
+            v[i][j]=c;
+            for (int k=0;k<4;k+=1)
+            {
+                int c1=i+v1[k],c2=j+v2[k];
+                if (c1>=0 && c1<n && c2>=0 && c2<m && visit[c1][c2]==0)
                 {
-                    if (i>0) dp[i][j]=min(dp[i][j],dp[i-1][j]+1);
-                    if (j>0) dp[i][j]=min(dp[i][j],dp[i][j-1]+1);
+                    visit[c1][c2]=1;
+                    q.push({{c1,c2},c+1});
                 }
             }
         }
-        for (int i=mat.size()-1;i>=0;i-=1)
-        {
-            for (int j=mat[0].size()-1;j>=0;j-=1)
-            {
-                if (mat[i][j]==0) dp[i][j]=0;
-                else
-                {
-                    if (i<mat.size()-1) dp[i][j]=min(dp[i][j],dp[i+1][j]+1);
-                    if (j<mat[0].size()-1) dp[i][j]=min(dp[i][j],dp[i][j+1]+1);
-                }
-            }
-        }
-        return dp;
+        return v;
     }
 };
