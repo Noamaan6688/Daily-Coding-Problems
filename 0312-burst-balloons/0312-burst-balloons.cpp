e3d52ea1ac30be488fb25ac32938571ce49edc88
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-24 13:17:50 +0530

    Time: 345 ms (85.11%), Space: 10.3 MB (63.39%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -13,8 +13,19 @@ public:
         nums.insert(nums.begin(),1);
         nums.insert(nums.end(),1);
         int n=nums.size()-2;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int c=df(1,n,nums,dp);
-        return c;
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for (int i=n;i>=1;i-=1)
+        {
+            for (int j=i;j<=n;j+=1)
+            {
+                int c=INT_MIN;
+                for (int k=i;k<=j;k+=1)
+                {
+                    c=max(c,nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j]);
+                }
+                dp[i][j]=c;
+            }
+        }
+        return dp[1][n];
     }
 };
