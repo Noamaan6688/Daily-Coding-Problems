7e48e4013834eacb61304db68f5cb08840859393
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-25 15:32:21 +0530

    Time: 50 ms (30.02%), Space: 13.6 MB (57.14%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -19,7 +19,27 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& fl, int src, int dst, int k) {
         vector<vector<int>>v(n+1,vector<int>(n+1,-1)),dp(k+2,vector<int>(n+1,-1));
         for (auto i:fl) v[i[0]][i[1]]=i[2];
-        int c=df(src,dst,k+1,n,v,dp);
+        // int c=df(src,dst,k+1,n,v,dp);
+        for (int k1=0;k1<k+2;k1+=1)
+        {
+            for (int i=n-1;i>=0;i-=1)
+            {
+                if (i==dst) dp[k1][i]=0;
+                else
+                {
+                    int c1=1e7;
+                    for (int j=0;j<n;j+=1)
+                    {
+                        if (v[i][j]!=-1)
+                        {
+                            if (k1>0) c1=min(c1,v[i][j]+dp[k1-1][j]);
+                        }
+                    }
+                    dp[k1][i]=c1;
+                }
+            }
+        }
+        int c=dp[k+1][src];
         if (c==1e7) return -1;
         return c;
     }
