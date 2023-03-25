744fe5089e80fbe875e1f2cea35a7a02fc19b432
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-25 14:46:32 +0530

    Time: 664 ms (53.66%), Space: 211.9 MB (25.77%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    void df(long long&i,long long&c1,vector<bool>&visit,vector<vector<long long>>&v)
+    {
+        if (visit[i]) return;
+        visit[i]=1;
+        c1+=1;
+        for (auto &j:v[i]) df(j,c1,visit,v);
+    }
+    
     long long countPairs(int n, vector<vector<int>>& edges) {
         vector<vector<long long>>v(n+1);
         for (auto i:edges)
@@ -7,30 +15,15 @@ public:
             v[i[0]].push_back(i[1]);
             v[i[1]].push_back(i[0]);
         }
-        queue<long long>q;
+        // queue<long long>q;
         vector<bool>visit(n,0);
         long long c=0,c1=0,c2=0;
         for (long long i=0;i<n;i+=1)
         {
             if (visit[i]==0)
             {
-                q.push(i);
-                while (!q.empty())
-                {
-                    int j=q.front();q.pop();
-                    visit[j]=1;
-                    c1+=1;
-                    for (auto k:v[j])
-                    {
-                        if (visit[k]==0)
-                        {
-                            q.push(k);
-                            visit[k]=1;
-                        }
-                    }
-                    c2=n-c1;
-                }
-                c+=c1*c2;
+                df(i,c1,visit,v);
+                c+=c1*(n-c1);
             }
             c1=0;
         }
