c0b8081a125d57e1b9e86ddc00c252bcb1058ba6
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-26 23:32:37 +0530

    Time: 2 ms (59.25%), Space: 6.6 MB (72.52%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -13,8 +13,19 @@ public:
     }
     
     int combinationSum4(vector<int>& nums, int ta) {
-        vector<int>dp(ta+1,-1);
-        int c=df(ta,nums,dp);
-        return c;
+        
+        vector<uint>dp(ta+1,0);
+        // int c=df(ta,nums,dp);
+        dp[0]=1;
+        for (int i=1;i<=ta;i+=1)
+        {
+            for (auto j:nums)
+            {
+                if (j<=i) dp[i]+=dp[i-j];
+            }
+        }
+        
+        
+        return dp[ta];
     }
 };
