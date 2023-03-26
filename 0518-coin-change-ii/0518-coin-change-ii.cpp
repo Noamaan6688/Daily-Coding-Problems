c74714de3cb97b6241768a258fc4d5ddb29abb30
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-26 23:21:23 +0530

    Time: 29 ms (53.62%), Space: 7.3 MB (83.55%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -16,23 +16,25 @@ public:
     
     int change(int am,vector<int>& co) {
         int n=co.size();
-        vector<vector<int>>dp(n+1,vector<int>(am+1,0));
+        // vector<vector<int>>dp(n+1,vector<int>(am+1,0));
         // int c=df(n-1,am,co,dp);
+        vector<int>prev1(am+1,0),prev2(am+1,0);
         for (int i=0;i<=am;i+=1)
         {
-            if (i%co[0]==0) dp[0][i]=1;
+            if (i%co[0]==0) prev2[i]=1;
         }
         for (int i=1;i<n;i+=1)
         {
             for (int j=0;j<=am;j+=1)
             {
-                int c1=dp[i-1][j];
+                int c1=prev2[j];
                 int c2=0;
-                if (j>=co[i]) c2=dp[i][j-co[i]];
-                dp[i][j]=c1+c2;
+                if (j>=co[i]) c2=prev1[j-co[i]];
+                prev1[j]=c1+c2;
             }
+            prev2=prev1;
         }
         
-        return dp[n-1][am];
+        return prev2[am];
     }
 };
