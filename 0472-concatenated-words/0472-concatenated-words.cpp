6800779d3231ad4d8c0057d60790a767e291201e
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-27 12:59:22 +0530

    Time: 199 ms (84.27%), Space: 49.5 MB (31.00%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -15,12 +15,28 @@ public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         vector<string>v;
         if (words.size()==0) return v;
-        unordered_map<string,bool>mp;
+        // unordered_map<string,bool>mp;
         unordered_set<string>st;
         for (auto i:words) st.insert(i);
+        
         for (auto i:words)
         {
-            if (df(i,st,mp)) v.push_back(i);
+            int n1=i.size();
+            vector<int>v1(n1+1,0);
+            v1[0]=1;
+            for (int j=0;j<n1;j+=1)
+            {
+                if (!v1[j]) continue;
+                for (int j1=j+1;j1<=n1;j1+=1)
+                {
+                    if (j1-j<n1 && st.count(i.substr(j,j1-j))) v1[j1]=1;
+                }
+                if (v1[n1])
+                {
+                    v.push_back(i);
+                    break;
+                }
+            }
         }
         return v;
     }
