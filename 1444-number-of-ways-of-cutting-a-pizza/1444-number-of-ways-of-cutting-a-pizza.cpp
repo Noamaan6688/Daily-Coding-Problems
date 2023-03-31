b732701bfc2322a8f2b2a5c3749ae3be02bea2ec
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-03-31 15:36:41 +0530

    Time: 24 ms (82.20%), Space: 8.5 MB (23.04%) - LeetHub

---
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int n, m;
+    int mod = 1e9 + 7;
+    vector<vector<vector<int>>>dp;
+    int df(vector<string> &a,int k,int i,int j)
+    {
+        if(k==1)
+        {
+            for(int ti=i;ti<=n-1;ti+=1)
+            {
+                for(int tj=j;tj<=m-1;tj+=1)
+                {
+                    if (a[ti][tj]=='A') return 1;
+                }
+            }
+            return 0;
+        }
+        if(dp[i][j][k]!=-1) return dp[i][j][k];
+        int res = 0;
+        int flag = 0;
+        for(int indx=i+1;indx<=n-1;indx+=1)
+        {
+            if(flag==0)
+            {
+                for(int tj=j;tj<=m-1;tj+=1)
+                {
+                    if (a[indx-1][tj]=='A')
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+            if(flag) res=(res+df(a,k-1,indx,j))%mod;
+        }
+        flag=0;
+        for(int indx=j+1;indx<=m-1;indx+=1)
+        {
+            if(flag==0)
+            {
+                for(int ti=i;ti<=n-1;ti+=1)
+                {
+                    if(a[ti][indx-1]=='A')
+                    {
+                        flag=1;
+                        break;
+                    }
+                }
+            }
+            if(flag) res=(res+df(a,k-1,i,indx))%mod;
+        }
+
+        return dp[i][j][k]=res%mod;
+    }
+    int ways(vector<string>&grid,int k) {
+        n=grid.size();m=grid[0].size();
+        dp.resize(n+1,vector<vector<int>>(m+1, vector<int> (k+1, -1)));
+        return df(grid,k,0,0);
+    }
+};
