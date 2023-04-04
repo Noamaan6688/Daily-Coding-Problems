ca106ee42f32c4417ebc094f5c0feeb43075d9a2
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-04-04 23:53:10 +0530

    Time: 6 ms (86.83%), Space: 7 MB (60.82%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int j=0,c=0,c1=0;
-        unordered_map<char,int>mp;
-        for(int i=0;i<s.size();i+=1)
+        if (s.size()==0) return 0;
+        vector<int>v(200);
+        int j=0,c=0;
+        for (int i=0;i<s.size();i+=1) 
         {
-            mp[s[i]]++;
-            c1=max(c1,mp[s[i]]);
-            if(i-j+1-c1>k)
-            {
-                mp[s[j]]--;
-                j+=1;
-            }
-            c=max(c,i-j+1);
+            v[s[i]]+=1;
+            c=max(c,v[s[i]]);
+            if (i-j+1-c>k) v[s[j]]--,j+=1;
         }
-        return c;
+        return s.size()-j;
     }
 };
