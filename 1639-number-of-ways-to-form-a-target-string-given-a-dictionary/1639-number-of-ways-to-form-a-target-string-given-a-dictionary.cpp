567fcd2573af1c8ac2e5e29a361c827325fcf628
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-04-16 14:16:23 +0530

    Time: 365 ms (39.66%), Space: 54.2 MB (46.12%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     long long MOD = 1000000007;
-    int df(vector<vector<int>>&v,vector<vector<int>>&dp, string& ta, int i, int j)
-    {
-        if (j==ta.size()) return 1; 
-        if (i==v.size()) return 0;  
-        if (dp[i][j]!=-1) return dp[i][j];
-        char ch=ta[j];
-        int c1=df(v,dp,ta,i+1,j);
-        int c2=0;
-        if (v[i][ch-'a']!=0)
-        {
-            int c3=v[i][ch-'a'];
-            int c4=df(v,dp,ta,i+1,j+1);
-            c2=(c3*(long long)c4)%MOD;
-        }
-        return dp[i][j]=((c2+(long long)c1)%MOD);
+//     int df(vector<vector<int>>&v,vector<vector<int>>&dp, string& ta, int i, int j)
+//     {
+//         if (j==ta.size()) return 1; 
+//         if (i==v.size()) return 0;  
+//         if (dp[i][j]!=-1) return dp[i][j];
+//         char ch=ta[j];
+//         int c1=df(v,dp,ta,i+1,j);
+//         int c2=0;
+//         if (v[i][ch-'a']!=0)
+//         {
+//             int c3=v[i][ch-'a'];
+//             int c4=df(v,dp,ta,i+1,j+1);
+//             c2=(c3*(long long)c4)%MOD;
+//         }
+//         return dp[i][j]=((c2+(long long)c1)%MOD);
         
-    }
+//     }
     int numWays(vector<string>& words, string ta) 
     {
         int m=words.size(),n=words[0].size(),n1=ta.size();
@@ -51,7 +51,7 @@ public:
                 if (v[i][ch-'a']!=0)
                 {
                     int c3=v[i][ch-'a'];
-                    int c4=df(v,dp,ta,i+1,j+1);
+                    int c4=dp[i+1][j+1];
                     c2=(c3*(long long)c4)%MOD;
                 }
                 dp[i][j]=((c2+(long long)c1)%MOD);
