bf3e9446a68bfcdb17d6879b82f3ac9ea4e2fccc
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-04-16 14:15:15 +0530

    Time: 405 ms (28.88%), Space: 54.2 MB (46.12%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -20,7 +20,7 @@ public:
     }
     int numWays(vector<string>& words, string ta) 
     {
-        int m=words.size(),n=words[0].size();
+        int m=words.size(),n=words[0].size(),n1=ta.size();
         vector<vector<int>>v(n, vector<int>(26, 0));
         vector<vector<int>>dp(n+1,vector<int>(ta.size()+1,-1));
         for (int i=0;i<m;i+=1)
@@ -31,7 +31,32 @@ public:
                 v[j][ch-'a']+=1; 
             }
         }
-        int c=df(v,dp,ta,0, 0);
-        return c;
+        for (int i=n;i>=0;i-=1)
+        {
+            for (int j=n1;j>=0;j-=1)
+            {
+                if (j==n1)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+                if (i==n)
+                { 
+                    dp[i][j]=0;
+                    continue;
+                }
+                char ch=ta[j];
+                int c1=dp[i+1][j];
+                int c2=0;
+                if (v[i][ch-'a']!=0)
+                {
+                    int c3=v[i][ch-'a'];
+                    int c4=df(v,dp,ta,i+1,j+1);
+                    c2=(c3*(long long)c4)%MOD;
+                }
+                dp[i][j]=((c2+(long long)c1)%MOD);
+            }
+        }
+        return dp[0][0];
     }
 };
