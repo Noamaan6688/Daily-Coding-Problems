d9cd55ad019a808b296da2db8e08e1f77ab7f506
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-04-23 15:20:43 +0530

    Time: 46 ms (91.86%), Space: 11.8 MB (91.86%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -19,8 +19,21 @@ public:
     
     
     int numberOfArrays(string s, int k) {
-        vector<int>dp(s.size()+1,-1);
-        int c=df(0,k,s,dp);
-        return c;
+        vector<int>dp(s.size()+1,0);
+        dp[s.size()]=1;
+        for (int i=s.size()-1;i>=0;i-=1)
+        {
+            long long int c=0,c1=0;
+            for (int j=i;j<s.size();j+=1)
+            {
+                int c2=s[j]-'0';
+                c1=c1*10+c2;
+                if (c1<1 || c1>k) break;
+                c=(c+dp[j+1])%mod;
+            }
+            dp[i]=c;
+        }
+        // int c=df(0,k,s,dp);
+        return dp[0];
     }
 };
