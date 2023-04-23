41e7aee0b04ff97712e299adf7bec0374671548b
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-04-23 15:16:17 +0530

    Time: 88 ms (33.14%), Space: 21.9 MB (25.00%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mod=1000000007;
+    int df(int i,int k,string&s,vector<int>&dp)
+    {
+        if (i==s.size()) return 1;
+        if (dp[i]!=-1) return dp[i];
+        long long int c=0,c1=0;
+        for (int j=i;j<s.size();j+=1)
+        {
+            int c2=s[j]-'0';
+            c1=(c1*10)+c2;
+            if (c1<1 || c1>k) break;
+            int c3=df(j+1,k,s,dp)%mod;
+            c=(c+c3)%mod;
+        }
+        return dp[i]=c;
+    }
+    
+    
+    int numberOfArrays(string s, int k) {
+        vector<int>dp(s.size()+1,-1);
+        int c=df(0,k,s,dp);
+        return c;
+    }
+};
