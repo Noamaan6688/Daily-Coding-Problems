95563b4aa6384e9a65e69525139fc0913998dec8
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-04-27 14:21:56 +0530

    Time: 3 ms (45.34%), Space: 5.9 MB (63.99%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        long long int i=1,j=n,c=0;
+        while (i<=j)
+        {
+            long long int m=(i+j)/2;
+            if (m*m==n) return m;
+            if (m*m<n) i=m+1;
+            else j=m-1;
+        }
+        return i-1;
+    }
+};
