a75e400a44b45aa4eeeb80c5cd5f32a7ca0f7487
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-04-28 23:47:57 +0530

    Time: 363 ms (20.31%), Space: 11.5 MB (45.71%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
-     bool df(vector<string>&v,string s)
+     bool df(string&s1,string&s2)
     {
-        for(int i=0;i<v.size();i+=1)
+        int c=0;
+        for (int i=0;i<s1.size();i+=1)
         {
-            int c=0;
-            string s1=v[i];
-            if (s.length()!=s1.length()) return 0;
-            if(s==s1) return 1;
-            for (int i=0;i<s1.size();i+=1)
-            {
-                if (s[i]!=s1[i]) c+=1;
-            }
-            if (c==2) return 1;
+            if (s1[i]!=s2[i]) c+=1;
         }
+        if (c==2 || c==0) return 1;
         return 0;
     }
     
-    int numSimilarGroups(vector<string>& s) 
+    int numSimilarGroups(vector<string>&s) 
     {
-         int c=0;
-         vector<string>v;
-         vector<vector<string>>v1; 
-         v.push_back(s[0]);
-         v1.push_back(v);
-         for (int i=1;i<s.size();i+=1)
-         {
-             string s1=s[i];
-             int j=0,c1=0,c2=-1;
-             for (j=0;j<v1.size();j+=1)
-             {
-                 if (df(v1[j],s1)==1)
-                 {
-                     v1[j].push_back(s1);
-                     c2+=1;
-                     c1=1;
-                 }
-
-             }
-             if(!c1)
-             {
-                 v1.push_back({s1});
-                 continue;
-             }
-             c+=c2;
-         } 
-        if (c<=v1.size())return v1.size()-c;
-        return 1;
+        int c=0,n=s.size();
+        vector<bool>visit(n+1,0);
+        queue<string>q;
+        for (int i=0;i<n;i+=1)
+        {
+            if (visit[i]==0)
+            {
+                visit[i]=1;
+                c+=1;
+                q.push(s[i]);
+                while (q.size())
+                {
+                    string s1=q.front();q.pop();
+                    for (int j=0;j<n;j+=1)
+                    {
+                        if (df(s1,s[j]) && visit[j]==0) visit[j]=1,q.push(s[j]);
+                    }
+                }
+            }
+        }
+        return c;
     }
 };
