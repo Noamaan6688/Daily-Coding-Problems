4ed876bc1af5a178831679e14fed7e7796711148
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-05-11 17:56:25 +0530

    Time: 19 ms (67.78%), Space: 12.9 MB (63.96%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -17,8 +17,24 @@ public:
     
     
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>>dp(nums1.size()+1,vector<int>(nums2.size(),-1));
-        int c=df(0,0,nums1,nums2,dp);
+        vector<vector<int>>dp(nums1.size()+1,vector<int>(nums2.size()+1,0));
+        int n1=nums1.size(),n2=nums2.size();
+        for (int i=n1-1;i>=0;i-=1)
+        {
+            for (int j=n2-1;j>=0;j-=1)
+            {
+                int c1=0,c2=0;
+                if (nums1[i]==nums2[j]) c1=1+dp[i+1][j+1];
+                else
+                {
+                    int c3=dp[i+1][j],c4=dp[i][j+1];
+                    c2=max(c3,c4);
+                }
+                dp[i][j]=max(c1,c2);
+            }
+        }
+        
+        int c=dp[0][0];
         return c;
     }
 };
