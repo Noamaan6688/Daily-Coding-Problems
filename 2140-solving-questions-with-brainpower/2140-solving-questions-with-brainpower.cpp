f9a735d478be85736820fccdf875ae0606b2269e
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-05-13 19:06:30 +0530

    Time: 404 ms (20.63%), Space: 115.2 MB (83.86%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -14,8 +14,18 @@ public:
     
     long long mostPoints(vector<vector<int>>& qu) {
         int n=qu.size();
-        vector<long long>dp(n+1,-1);
-        return df(0,n,qu,dp);
+        vector<long long>dp(n+1,0);
+        dp[n-1]=qu[n-1][0];
+        long long c1=0,c2=0;
+        for (int i=n-1;i>=0;i-=1)
+        {
+            c1=0;c2=qu[i][0];
+            if (i+1<n) c1+=dp[i+1];
+            if (i+qu[i][1]+1<n) c2+=dp[i+qu[i][1]+1];
+            dp[i]=max(c1,c2);
+        }
+        
+        return dp[0];
         
     }
 };
