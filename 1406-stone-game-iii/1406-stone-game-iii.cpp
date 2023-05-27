d126197076669a752ed6fa8b9f3ab4d888e6c48b
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-05-27 12:37:08 +0530

    Time: 253 ms (72.51%), Space: 132.1 MB (86.26%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -19,17 +19,19 @@ public:
         int n = stoneValue.size();
         vector<int> dp(n + 1, 0);
         dp[n] = 0;
+        int prev1=0,prev2=0,prev3=0;
         for (int i = n - 1; i >= 0; i--)
         {
             int score = INT_MIN;
-            score = max(score, stoneValue[i] - dp[i + 1]);
+            score = max(score, stoneValue[i] - prev3);
             if (i + 1 < stoneValue.size())
-                score = max(score, stoneValue[i] + stoneValue[i + 1] - dp[i + 2]);
+                score = max(score, stoneValue[i] + stoneValue[i + 1] - prev2);
             if (i + 2 < stoneValue.size())
-                score = max(score, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3]);
-            dp[i] = score;
+                score = max(score, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - prev1);
+            prev1=prev2;
+            prev2=prev3;
+            prev3=score;
         }
-        return (dp[0] > 0) ? "Alice" : dp[0] == 0 ? "Tie"
-                                                  : "Bob";
+        return (prev3 > 0) ? "Alice" : prev3 == 0 ? "Tie": "Bob";
     }
 };
