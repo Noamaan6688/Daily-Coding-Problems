b763879179f1fbe00e63bb27b975e772e55fef43
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-05-29 20:15:17 +0530

    Time: 60 ms (74.17%), Space: 33.3 MB (28.39%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -0,0 +1,20 @@
+class ParkingSystem {
+public:
+    unordered_map<int,int>mp;
+    ParkingSystem(int big, int medium, int small) {
+        mp[1]=big;
+        mp[2]=medium;
+        mp[3]=small;
+    }
+    
+    bool addCar(int carType) {
+        mp[carType]--;
+        return mp[carType]>=0; 
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
