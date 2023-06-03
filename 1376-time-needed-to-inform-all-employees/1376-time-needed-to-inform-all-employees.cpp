a8b62e5da69a965c07fb589e5a0e041ae7a6e6a0
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-06-03 16:55:39 +0530

    Time: 193 ms (92.07%), Space: 92.9 MB (90.83%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,30 +1,21 @@
 class Solution {
-    int n1, headID1;
-    vector<int> informTime1;
-    vector<vector<int>> graph;
-    
-    int dfs(int i, vector<int> &vis){
-        vis[i] = 1;
-        
-        int time = 0;
-        for(auto j : graph[i])
-        {
-            if(vis[j] == 0)time = max(time, dfs(j, vis));
-        }
-        
-        time += informTime1[i];
-        return time;
+    vector<int> dp;
+    int dfs(int i, int headID, vector<int> &manager, vector<int>& informTime){
+        if(i == -1) return 0;
+
+        if(dp[i] != -1) return dp[i];
+        int ans = informTime[i] + dfs(manager[i], headID, manager, informTime); 
+        return dp[i] = ans;
     }
-    
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        n1 = n, headID1 = headID, informTime1 = informTime;
-        graph = vector<vector<int>>(n);
-        for(int i = 0; i<n; i++){
-            if(i != headID)
-                graph[manager[i]].push_back(i);
+        dp = vector<int>(n, -1);
+        int ans = 0;
+        for(int i = 0; i<n; i++)
+        {
+            if(informTime[i] == 0) ans = max(ans, dfs(i, headID, manager, informTime));
         }
-		vector<int> vis(n, 0);
-        return dfs(headID, vis);
+
+        return ans;
     }
 };
