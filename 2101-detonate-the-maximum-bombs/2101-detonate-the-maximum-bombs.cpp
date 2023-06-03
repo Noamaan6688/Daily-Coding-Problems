edbf9c12a2dab9831765b65807f5292772c37c73
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-06-03 16:15:26 +0530

    Time: 557 ms (20.43%), Space: 57.3 MB (12.90%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,60 +1,35 @@
 class Solution {
 public:
-    typedef long long LL;
-    void DFS(int u, unordered_set<int> & visited, unordered_map<int, vector<int>> &adj) {
-        
-        visited.insert(u);
-        
-        for(int &v : adj[u]) {
-            if(visited.find(v) == visited.end()) { //Not visited
-                DFS(v, visited, adj);
-            }
-        }
-
-    }
-    
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n = bombs.size();
-        
-        unordered_map<int, vector<int>> adj;
-        
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<n; j++) {
-                
-                if(i == j) //same bomb
-                    continue;
-                
-                LL x1 = bombs[i][0];
-                LL y1 = bombs[i][1];
-                LL r1 = bombs[i][2];
-                
-                LL x2 = bombs[j][0];
-                LL y2 = bombs[j][1];
-                LL r2 = bombs[j][2];
-                
-                //Make a directed edge from i to j if i can detonate j as well
-                
-                LL distance = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
-                
-                if(LL(r1*r1) >= distance) {
-                    adj[i].push_back(j);
+        queue<vector<int>> q;
+
+        int count = 0;
+        int ans = 0;
+        for(int i = 0; i < n ; i++)
+        {
+            vector<bool> detonated(n,false);
+            q.push(bombs[i]);
+            detonated[i] = true;
+            count = 1;
+            while(!q.empty())
+            {
+                auto it = q.front();
+                q.pop();
+                for(int j = 0; j  < n; j++)
+                {
+                    if(!detonated[j] && sqrt(pow(bombs[j][0] - it[0], 2) + pow(bombs[j][1] - it[1],2)) <= it[2]){
+                        detonated[j] = true;
+                        q.push(bombs[j]);
+                        count++;
+                    };
                 }
-                
             }
+
+            ans = max(ans, count);
+                
         }
-        
-        
-        int result = 0;
-        unordered_set<int> visited;
-        
-        for(int i = 0; i<n; i++) {
-            DFS(i, visited, adj);
-            int count = visited.size();
-            result = max(result, count);
-            visited.clear();
-        }
-        
-        return result;
+        return ans;
         
     }
 };
