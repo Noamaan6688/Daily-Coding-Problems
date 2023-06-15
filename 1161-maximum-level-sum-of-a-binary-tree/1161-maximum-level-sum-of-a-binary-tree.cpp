7ddcb93b2247bea4ce59c5909ad8c69d86a11977
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-06-15 20:13:43 +0530

    Time: 212 ms (40.63%), Space: 107.4 MB (13.54%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -12,27 +12,28 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        int c=0,c1=-1e6,i=0;
-        queue<TreeNode*>q;
-        q.push(root);
-        while (!q.empty())
-        {
-            i+=1;
-            int c2=0,n=q.size();
-            for (int j=0;j<n;j+=1)
-            {
-                TreeNode*c3=q.front();
-                q.pop();
-                c2+=c3->val;
-                if (c3->left) q.push(c3->left);
-                if (c3->right) q.push(c3->right);
+        queue<TreeNode*> Q;
+        if (root == nullptr) return 0;
+        Q.push(root);
+        int level = 0;
+        map<int, int> sum;
+        while (!Q.empty()) {
+            int cursum = 0;
+            int count = Q.size();
+            level ++;
+            for (int i = 0; i < count; ++ i) {
+                auto p = Q.front();
+                cursum += p->val;
+                if (p->left != nullptr) Q.push(p->left);
+                if (p->right != nullptr) Q.push(p->right);
+                Q.pop();
             }
-            if (c2>c1)
-            {
-                c=i;
-                c1=c2;
+            if (sum.find(cursum) == sum.end()) {
+                sum[cursum] = level;
+            } else {
+                sum[cursum] = min(sum[cursum], level);
             }
         }
-        return c;
+        return rbegin(sum)->second;
     }
 };
