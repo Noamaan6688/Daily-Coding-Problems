3e4fc6a0ac5da8f45fb233eba754643550a4831a
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-06-20 23:41:47 +0530

    Time: 245 ms (80.82%), Space: 129.8 MB (79.40%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-         int n=nums.size();
-         vector<long long int> pre(n),suff(n);
-         pre[0]=nums[0];
-         for(int i=1;i<n;i+=1) pre[i]=pre[i-1]+nums[i];
-         suff[n-1]=nums[n-1];
-         for(int i=n-2;i>=0;i-=1) suff[i]=suff[i+1]+nums[i];
-         vector<int>ans;
-        for(int i=0;i<n;i+=1)
+        int n=nums.size();
+        long long int s=0;
+        vector<int> ans(n,-1);
+        for (int i=0;i<n;i+=1)
         {
-            if(i-k<0 || i+k>=n) ans.push_back(-1);
+            if (i<2*k) s+=nums[i];
             else
             {
-                long long int sum = (long long int)((suff[i-k]-suff[i]) + (pre[i+k]-pre[i]) + nums[i]);
-                sum=sum/(2*k+1);
-                ans.push_back(sum);
+                s+=nums[i];
+                ans[i-k] = s/(2*k+1);
+                s-= nums[i-(2*k)];
             }
         }
- 
         return ans;
     }
 };
