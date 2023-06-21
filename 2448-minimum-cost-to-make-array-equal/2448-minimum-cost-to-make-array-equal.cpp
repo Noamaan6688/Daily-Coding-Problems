39dad16f9dc7c3fe971c0f0197b4294b108e4895
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-06-21 20:36:14 +0530

    Time: 137 ms (23.72%), Space: 41.6 MB (42.34%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
-    long long minCost(vector<int>& nums, vector<int>& cost) {
-        long long ans=LLONG_MAX;
-        vector<long long> pref;
-        vector<pair<long, long>> p;
-        
-        for(int i=0; i<cost.size(); i++){
-            p.push_back({nums[i], cost[i]});
-        }
-        
-        sort(p.begin(), p.end());
-        
-        pref.push_back(0);
-        for(int i=0; i<p.size(); i++){
-            pref.push_back(pref[i]+p[i].second);
-        }
-        
-        long long left=0, right=0;
-        for(int i=0; i<nums.size(); i++){
-           
-            if(i==0){
-                left=0;
-                for(int j=1; j<cost.size(); j++){
-                    right+=(long long)(p[j].first-p[0].first)*(long long)p[j].second;
-                }
-                if(ans>right+left){
-                    ans=left+right;
-                }
-                continue;
+    long long mod(int x){
+        if(x>0) return x;
+        return -x;
+    }
+    
+    long long minCost(vector<int>& nums, vector<int>& costs) {
+        auto findCost = [&](long long pivot) -> long long{
+            long long ans=0;
+            for(int j=0;j<nums.size();j++){
+                long long diff = mod(nums[j]-pivot);
+                ans+=diff*(long long)costs[j];     
             }
-            long long elevation=p[i].first-p[i-1].first;
-            right-=elevation*(long long)(p[i].second);
-            right-=elevation*(pref[pref.size()-1]-pref[i+1]);
-            left+=elevation*(pref[i]-pref[0]);
-            if(ans>right+left) ans=left+right;
+
+            return ans;
+        };
+        
+        vector<pair<int,int>>arr;
+        int n=nums.size();
+        for(int i=0;i<n;i++)arr.push_back({nums[i],costs[i]});
+        sort(arr.begin(),arr.end());
+        int low=0, high=n-1;
+        
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(findCost(arr[mid].first)< findCost(arr[mid].first +1))high=mid;
+            else low=mid+1;
         }
         
-        return ans;
+        return findCost(arr[low].first);
     }
 };
