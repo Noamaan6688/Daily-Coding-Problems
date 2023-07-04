707a9015d3ad552c0c317944713b84170f5787cb
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-07-04 20:23:16 +0530

    Time: 12 ms (35.14%), Space: 9.3 MB (98.52%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=0; i<32; i++)
+        {     
+            int t=0;
+            for(int j=0; j<nums.size(); j++)
+            {
+                t+=(nums[j]&1);
+                nums[j]>>=1;
+            }
+            ans+=((t%3)<<i);
+        }
+        return ans;
+    }
+};
