ea175791c312f2fdb0d1e516fbd921e0c861514d
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-07-08 21:57:20 +0530

    Time: 213 ms (58.08%), Space: 59.8 MB (95.88%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -2,27 +2,13 @@ class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
      int n=weights.size();
-        if (n<=2) return 0;
-
-        priority_queue<int> maxHeap;
-        priority_queue<int, vector<int>, greater<int> > minHeap;
-        long long maxP=0, minP=0;
-        for (int i=0; i<k-1; i++){
-            int partition=weights[i]+weights[i+1];
-            maxP+=partition;
-            maxHeap.push(partition);
-            minHeap.push(partition);
-        }
-        minP=maxP; 
-        for(int i=k-1; i<n-1; i++){
-            int partition=weights[i]+weights[i+1];
-            maxHeap.push(partition);
-            minP+=partition-maxHeap.top();
-            maxHeap.pop();
-            minHeap.push(partition);
-            maxP+=partition-minHeap.top();
-            minHeap.pop();
-        }
+        if (n<=2 || n==k) return 0;
+        vector<int> P(n-1, 0);
+        for (int i=0; i<n-1; i++) P[i]=weights[i]+weights[i+1];
+        sort(P.begin(), P.end());
+        long long minP=accumulate(P.begin(), P.begin()+(k-1), 0LL), 
+        maxP=accumulate(P.begin()+(n-k), P.end(), 0LL);
+        
         return maxP-minP;
     }
 };
