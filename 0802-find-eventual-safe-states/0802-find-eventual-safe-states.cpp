13c9760adab823e70baf1e8cece0952978a13e2c
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-07-12 22:57:52 +0530

    Time: 231 ms (32.99%), Space: 61.7 MB (15.81%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -24,19 +24,24 @@ public:
  
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        
-        vector<bool> visited(n,false), dfsVisited(n,false);
-        set<int> cycle; 
+        vector<vector<int>> rev_graph(n);
+        vector<int> out_deg(n,0);
         for(int i=0;i<n;i++){
-            if(visited[i]==false){
-                if(dfs(graph,i,visited,dfsVisited,cycle)) cycle.insert(i);
+            out_deg[i] += graph[i].size();
+            for(int v : graph[i]) rev_graph[v].push_back(i);
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++) if(out_deg[i]==0) q.push(i);
+        vector<bool> is_safe(n,false);
+        while(q.size()>0){
+            int node = q.front();q.pop();
+            is_safe[node]=true;
+            for(int child : rev_graph[node]){
+                if(--out_deg[child] == 0) q.push(child);
             }
         }
-        
-		vector<int> ans;
-        for(int i=0;i<n;i++){
-            if(cycle.find(i)==cycle.end()) ans.push_back(i);
-        }
+        vector<int> ans;
+        for(int i=0;i<n;i++) if(is_safe[i]) ans.push_back(i);
         return ans;
     }
 };
