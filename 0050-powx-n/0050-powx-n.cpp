fcabfbbe811087039378a81b86e2e00ed1490b0e
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-07-24 22:04:37 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (28.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    double solve(double x,int n){
-        if(n==0) return 1;
-        double temp=myPow(x,n/2);
-        temp=temp*temp;
-        if(n%2==0) return temp;
-        else return temp*x;
+    double solve(double x,long long n){
+        double ans=1;
+        while(n>0)
+        {
+            int c=n%2;
+            if(c==1) ans=ans*x;
+            x=x*x;
+            n=n>>1;
+        }
+        return ans;
     }
     double myPow(double x, int n) {
-        double ans=solve(x,abs(n));
-        if (n<0) return 1/ans;
+         if(x == 1) return 1;
+         double ans=solve(x,abs(n));
+         
+        if(n<0) return 1/ans;
+         
         return ans;
-       
     }
 };
