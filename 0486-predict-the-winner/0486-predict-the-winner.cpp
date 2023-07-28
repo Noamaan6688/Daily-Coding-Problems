7eb722273c4a91ee3be6b9108eb5e4c442bc662e
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-07-28 21:43:23 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-    vector<int> dp(n, 0);
+    vector<vector<int>> dp(n, vector<int>(n, 0));
 
-   for (int i = 0; i < n; i++) {
-        dp[i] = nums[i];
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = nums[i];
     }
 
-   for (int len = 2; len <= n; len++) {
+    for (int len = 2; len <= n; len++) {
         for (int i = 0; i <= n - len; i++) {
             int j = i + len - 1;
- dp[i] = max(nums[i] - dp[i + 1], nums[j] - dp[i]);
+   dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+
         }
     }
-
-    return dp[0] >= 0;
+    return dp[0][n - 1] >= 0;
     }
 };
