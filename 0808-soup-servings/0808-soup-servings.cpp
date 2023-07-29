3581e422198de661177cadc2947c2debfb2b5b03
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-07-29 23:18:30 +0530

    Time: 337 ms (8.37%), Space: 138.4 MB (24.33%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    double f(int a, int b, vector<vector<double>>&dp){
-        if(a == 0 && b == 0) return 0.5;
-        if(a == 0) return 1;
-        if(b == 0) return 0;
-        if(dp[a][b] != -1) return dp[a][b];
-        double x = 0.25 * f(max(0, a - 100), b, dp);
-        double y = 0.25 * f(max(0, a - 75), max(0, b-25), dp);
-        double z = 0.25 * f(max(0, a - 50), max(0, b-50), dp);
-        double w = 0.25 * f(max(0, a - 25), max(0, b-75), dp);
-        return dp[a][b] = x + y + z + w;
-    }
-    
     double soupServings(int n) {
         if(n > 4800) return 1;
-        vector<vector<double>>dp(n+1, vector<double>(n+1, -1));
-        return f(n, n, dp);
+        vector<vector<double>> dp(n+1, vector<double>(n+1, 0));
+        dp[0][0] = 0.5;
+        for(int i=1; i<=n; i++) dp[0][i] = 1;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                double x = 0.25 * dp[max(0, i-100)][j];
+                double y = 0.25 * dp[max(0, i-75)][max(0, j-25)];
+                double z = 0.25 * dp[max(0, i-50)][max(0, j-50)];
+                double w = 0.25 * dp[max(0, i-25)][max(0, j-75)];   
+                dp[i][j] = x + y + w + z;
+            }
+        }
+        return dp[n][n];
     }
 };
