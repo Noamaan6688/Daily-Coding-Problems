a3bd325a75425900bd818a1969b94cfbcf2a34bb
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2023-08-02 23:14:55 +0530

    Time: 2 ms (68.70%), Space: 7.7 MB (60.28%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,22 +2,10 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        findPermu(nums, 0, res);
+        sort(begin(nums), end(nums));
+        do res.push_back(nums);
+        while (next_permutation(nums.begin(),nums.end()));
+
         return res;
-    }
-    
-    void findPermu(vector<int>& nums, int startIndx, vector<vector<int>> & res){
-        int sz = nums.size();
-        if(startIndx == sz){
-           vector<int> currPer;
-            for(auto num : nums)currPer.push_back(num);
-            res.push_back(currPer);
-            return;
-        }
-        for(int leftPtr = startIndx ; leftPtr < sz; leftPtr++){
-                swap(nums[startIndx], nums[leftPtr]);
-                findPermu(nums, startIndx+1, res);
-                swap(nums[startIndx], nums[leftPtr]);
-        }
     }
 };
