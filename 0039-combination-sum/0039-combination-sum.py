3d304f04f6356dab3df2d3555aea71cb332c26cd
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2024-09-17 19:35:24 +0530

    Time: 61 ms (22.57%), Space: 16.7 MB (43.80%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def combinationSum(self, can: List[int], tar: int) -> List[List[int]]:
         l=[]
-        def fun(n,l1,tar):
+        def fun(i,l1,tar):
             if tar==0:
-                l.append(l1)
+                l.append(l1[:])
                 return
-            if tar<0:
+            if tar<0 or i>=len(can):
                 return
-            for i in range(n,len(can)):
-                fun(i,l1+[can[i]],tar-can[i])
-            # fun(i,l1+[can[i]],tar-can[i])
-            # fun(i-1,l1+[can[i]],tar-can[i])
-            # fun(i-1,l1,tar)
+            
+            l1.append(can[i])
+            fun(i,l1,tar-can[i])
+            l1.pop()
+            fun(i+1,l1,tar)
+            return l
             
         fun(0,[],tar)
         return l
