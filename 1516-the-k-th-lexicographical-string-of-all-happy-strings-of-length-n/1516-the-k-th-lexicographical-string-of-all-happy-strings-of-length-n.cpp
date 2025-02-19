4d39d03a3e5327ba39f23ce83a632ed044db26de
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2025-02-19 23:20:11 +0530

    Time: 17 ms (42.81%), Space: 17.4 MB (52.76%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        if (k>(3<<(n-1))) return "";
+        queue<string>q;
+        q.push("");
+        while (k) 
+        {
+            string curr=q.front();
+            q.pop();
+            for (char c='a';c<='c';c+=1)
+            {
+                if (curr.empty() || curr.back()!=c)
+                {
+                    q.push(curr+c);
+                    if (curr.size()+1==n) k-=1;
+                }
+                if (k==0) break;
+            }
+        }
+        return q.back();
+    }
+};
