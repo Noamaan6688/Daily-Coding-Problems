c0a7bb8229f37c65d30f345f32b1ba47499c918f
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2025-06-08 23:01:11 +0530

    Time: 253 ms (6.03%), Space: 17.5 MB (15.73%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,28 @@
+class Compare 
+{
+public:
+    bool operator()(int a, int b) 
+    {
+        return to_string(a) > to_string(b); 
+    }
+};
+
+class Solution 
+{
+public:
+    vector<int> lexicalOrder(int n) 
+    {
+        priority_queue<int, vector<int>, Compare> pq;
+        for (int i = 1; i <= n; ++i) 
+        {
+            pq.push(i);
+        }
+        vector<int> result;
+        while (!pq.empty()) 
+        {
+            result.push_back(pq.top());
+            pq.pop();
+        }
+        return result;
+    }
+};
