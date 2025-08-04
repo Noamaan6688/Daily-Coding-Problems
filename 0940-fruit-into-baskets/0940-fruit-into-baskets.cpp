c5dc16ceea07189d8024032e4e0f6b9036e54af8
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2025-08-04 21:20:40 +0530

    Time: 1 ms (98.01%), Space: 70 MB (96.26%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        if (n <= 2) return n;
+        int a = -1, b = -1;
+        int lastBCount = 0;
+        int currLen = 0, maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            int fruit = fruits[i];
+            if (fruit == a || fruit == b) {
+                currLen++;
+            } else {
+                currLen = lastBCount + 1;
+            }
+            if (fruit == b) {
+                lastBCount++;
+            } else {
+                a = b;
+                b = fruit;
+                lastBCount = 1;
+            }
+            maxLen = max(maxLen, currLen);
+        }
+        return maxLen;
+    }
+};
