65b410e1946799027d3557f50822a88c2f158aa0
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2025-10-03 20:46:48 +0530

    Time: 23 ms (83.18%), Space: 18.1 MB (64.9%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    int trapRainWater(vector<vector<int>>& hm) { 
-        priority_queue<pair<int,pair<int,int>> ,vector<pair<int,pair<int,int>>> ,greater<pair<int,pair<int,int>>> >pq;
-        int n=hm.size(),m=hm[0].size();
-        vector<vector<int>>visit(n,vector<int>(m,0));
-        for (int i=0;i<n;i+=1)
-        {
-            for (int j=0;j<m;j+=1)
-            {
-                if (i==0 || j==0 || i==n-1 || j==m-1)
-                {
-                    visit[i][j]=1;
-                    pq.push({hm[i][j],{i,j}});
+    int trapRainWater(vector<vector<int>>& height) {
+        int m = height.size(), n = height[0].size();
+        using T = pair<int,pair<int,int>>;
+        priority_queue<T, vector<T>, greater<T>> pq; 
+        vector<vector<int>> vis(m, vector<int> (n, 0));
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 || i == m-1 || j == 0 || j == n-1) {
+                    pq.push({height[i][j], {i, j}});
+                    vis[i][j] = 1;
                 }
             }
         }
-        vector<pair<int,int>>d={{0,1},{1,0},{-1,0},{0,-1}};
-        int c=0;
-        while (!pq.empty())
-        {
-            auto i=pq.top();pq.pop();
-            int c1=i.first,c2=i.second.first,c3=i.second.second;
-            for (int j=0;j<4;j+=1)
-            {
-                int x1=c2+d[j].first,y1=c3+d[j].second;
-                if (x1<0 || y1<0 || x1==n || y1==m || visit[x1][y1]) continue;
-                if (hm[x1][y1]<c1)
-                {
-                    c+=c1-hm[x1][y1];
-                    pq.push({c1,{x1,y1}});
+
+        auto valid = [&](int m, int n, int r, int c){
+            return (min(r,c) >= 0 && r < m && c < n);
+        };
+        vector<pair<int,int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        int water = 0;
+        while(!pq.empty()){
+            auto [ht, point] = pq.top();
+            auto [r, c] = point;
+            pq.pop();
+            for(auto[dr, dc] : dir){
+                int nr = r + dr, nc = c + dc;
+                if(valid(m, n, nr, nc) && !vis[nr][nc]){
+                    vis[nr][nc] = 1;
+                    if(height[nr][nc] < ht) water += ht - height[nr][nc];
+                    pq.push({max(ht, height[nr][nc]), {nr, nc}});
                 }
-                else pq.push({hm[x1][y1],{x1,y1}});
-                visit[x1][y1]=1;
             }
         }
-        return c;                                 
+
+        return water;
     }
 };
