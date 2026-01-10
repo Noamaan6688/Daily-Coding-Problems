3c086939776408e1d8cfb9a1a2ac4223a1e870cd
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2026-01-11 00:00:33 +0530

    Time: 7 ms (96.05%), Space: 9.6 MB (96.52%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-     int f(int m, int n, string s1, string s2, vector<vector<int>>& dp) {
-        if(m == 0 || n == 0) return 0;
-        if(dp[m][n] != -1) return dp[m][n];
-        if(s1[m-1] == s2[n-1]) return dp[m][n] = 2 * int(s1[m-1]) + f(m-1, n-1, s1, s2, dp);
-
-        return dp[m][n] = max(f(m-1, n, s1, s2, dp), f(m, n-1, s1, s2, dp));
-    }
     int minimumDeleteSum(string s1, string s2) {
-        int m = s1.size();
-        int n = s2.size();
-        // vector<vector<int>> dp(m+1, vector<int>(n+1,0));
-        vector<int>prev1(n+1,0),prev2(n+1,0);
-        int k = 0;
+        int n = s1.size(), m = s2.size();
+        vector<int> dp(m + 1, 0);
 
-        for(int i=0; i<m; i++){
-            k += int(s1[i]);
-        }
-        for(int i=0; i<n; i++){
-            k += int(s2[i]);
+        for (int j = m - 1; j >= 0; j--) {
+            dp[j] = dp[j + 1] + s2[j];
         }
 
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                if(s1[i-1] == s2[j-1]) prev1[j] = 2 * int(s1[i-1]) +prev2[j-1];
-                else prev1[j] = max(prev2[j], prev1[j-1]);
+        for (int i = n - 1; i >= 0; i--) {
+            int prev = dp[m];
+            dp[m] += s1[i];
+
+            for (int j = m - 1; j >= 0; j--) {
+                int temp = dp[j];
+                if (s1[i] == s2[j]) {
+                    dp[j] = prev;
+                } else {
+                    dp[j] = min(
+                        s1[i] + dp[j],
+                        s2[j] + dp[j + 1]
+                    );
+                }
+                prev = temp;
             }
-            prev2=prev1;
         }
-        return k - prev2[n];
+        return dp[0];
     }
 };
