e0db53fb7abf7e0c9734718aaafdcbe997c75c9e
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2026-02-01 00:28:17 +0530

    Time: 0 ms (100%), Space: 19.8 MB (62.61%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        if (target=='z') return letters[0];
-        for (int i=0;i<letters.size();i+=1)
-        {
-            if (letters[i]>target) return letters[i];
+        for(auto &c : letters){
+            if(c > target){
+                return c;
+            }
         }
         return letters[0];
     }
