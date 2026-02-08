9581e1e96b44132eef0ece572e6b60c05711e503
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2026-02-09 00:12:30 +0530

    Time: 0 ms (100%), Space: 23 MB (54.19%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,43 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int helper(TreeNode *root)
-    {
+    int dfs(TreeNode* root){
         if(root == NULL) return 0;
-        int left = helper(root -> left);
-        if(left == -1) return -1;
-        int right = helper(root -> right);
-        if(right == -1) return -1;
-        if(abs(left - right) > 1) return -1;
-        return 1 + max(left, right);
+        
+        int leftheight = dfs(root->left);
+        if(leftheight == -1) return -1;
+        
+        int rightheight = dfs(root->right);
+        if(rightheight == -1) return -1;
+        
+        if(abs(leftheight - rightheight) > 1) return -1;
+        
+        return max(leftheight, rightheight) + 1;
     }
-    
+
     bool isBalanced(TreeNode* root) {
-        if(root == NULL) return 1;
-        queue<TreeNode *> q;
-        q.push(root);
-        while(!q.empty())
-        {
-            int n=q.size();
-            for(int i=0;i<n;i++)
-            {
-                TreeNode*node=q.front();q.pop();
-                int c= helper(node);
-                if(c==-1) return 0;
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-        }
-        return 1;
+        return dfs(root) != -1;
     }
 };
