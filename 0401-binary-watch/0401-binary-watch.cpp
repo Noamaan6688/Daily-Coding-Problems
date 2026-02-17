dd305371f49b5b9b14fa10d6a08148db2a0dc42a
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2026-02-17 16:57:29 +0530

    Time: 0 ms (100%), Space: 9.4 MB (95.23%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    int df(int k)
-    {
-        int c=0;
-        while (k)
-        {
-            k=k&k-1;
-            c+=1;
-        }
-        return c;
-    }
-    
-    vector<string> readBinaryWatch(int to) {
-        vector<string>v;
-        string s;
-        for (int i=0;i<12;i+=1)
-        {
-            for (int j=0;j<60;j+=1)
-            {
-                if (df(i)+df(j)==to)
-                {
-                    string s1=to_string(i),s2;
-                    if (j>9) s2=to_string(j);
-                    else s2="0"+to_string(j);
-                    s=s1+":"+s2;
-                    v.push_back(s);
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ans;
+
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                    string time = to_string(h) + ":";
+                    if (m < 10) time += "0";
+                    time += to_string(m);
+                    ans.push_back(time);
                 }
             }
         }
-        return v;
+        return ans;
     }
 };
