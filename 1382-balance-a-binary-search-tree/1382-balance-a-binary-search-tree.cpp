bf82f7bfda27637c5a9b48d9a68ec0168dba7c16
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2026-02-28 14:58:01 +0530

    Time: 11 ms (68.49%), Space: 65.7 MB (61.3%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -1,38 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void df1(TreeNode*root,vector<int>&v)
-    {
-        if (root==NULL) return;
-        df1(root->left,v);
-        v.push_back(root->val);
-        df1(root->right,v);
+    void traverseInorder(TreeNode *root, vector<int> &inorder){
+        if(root == nullptr)
+            return;
+        traverseInorder(root->left, inorder);
+        inorder.push_back(root->val);
+        traverseInorder(root->right, inorder);
     }
-    
-    TreeNode*df2(int i,int j,vector<int>&v)
-    {
-        if (i>j) return NULL;
-        
-        int k=(i+j)>>1;
-        TreeNode*node=new TreeNode(v[k]);
-        node->left=df2(i,k-1,v);
-        node->right=df2(k+1,j,v);
-        return node;
+
+    TreeNode* inorderToBST(vector<int> &inorder, int low, int high){
+        if(low > high)
+            return nullptr;
+        int mid = (low + high) / 2;
+        TreeNode *left = inorderToBST(inorder, low, mid-1);
+        TreeNode *right = inorderToBST(inorder, mid+1, high);
+        return  new TreeNode(inorder[mid], left, right);
     }
-    
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int>v;
-        df1(root,v);
-        return df2(0,v.size()-1,v);
+        vector<int> inorder;
+        traverseInorder(root, inorder);
+        return inorderToBST(inorder, 0, inorder.size()-1);
     }
 };
