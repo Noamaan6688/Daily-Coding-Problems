fc3afc03f84f075d3a9ea4ba840230ca155a22e4
Noamaan Abdul Azeem <75433026+Noamaan6688@users.noreply.github.com>
2026-04-30 14:13:04 +0530

    Time: 3 ms (8.47%), Space: 9.1 MB (26.89%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) 
+    {
+        int left = 0, right = 0, blank = 0;
+
+        for (char c : moves) 
+        {
+            if (c == 'L') left++;
+            else if (c == 'R') right++;
+            else blank++; // '_'
+        }
+
+        return abs(left - right) + blank;
+    }
+};
